11b232b3fc68a742c79a155f7baa7ceb2dfce83a
rohiitgit <rohitcodes03@gmail.com>
2024-08-23 02:35:44 +0530

    improved logic and added tailwindcss support

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,63 @@
-import { Buffer } from "buffer";
 import { useState } from "react";
 import "./App.css";
-import { generateMnemonic, mnemonicToSeedSync } from "bip39";
-import { derivePath } from "ed25519-hd-key";
-import nacl from "tweetnacl";
 import { Keypair } from "@solana/web3.js";
-
-window.Buffer = window.Buffer || Buffer;
+import { ethers } from "ethers";
 
 function App() {
+  const [ethWallets, setEthWallets] = useState([]);
+  const [solWallets, setSolWallets] = useState([]);
 
-  const [account, setAccount] = useState(0);
+  function createEthWallet() {
+    const wallet = ethers.Wallet.createRandom();
 
+    console.log(wallet);
+    console.log("Ethereum Address:", wallet.address);
+    console.log("Mnemonic:", wallet.mnemonic.phrase);
+    console.log("Private Key:", wallet.privateKey);
+
+    const mnemonic = wallet.mnemonic.phrase;
+    const publicKey = wallet.address;
+    const privateKey = wallet.privateKey;
+    ethWallets.push({
+      mnemonic: mnemonic,
+      address: publicKey,
+      privateKey: privateKey,
+    });
+    setEthWallets([
+      ...ethWallets,
+      {
+        mnemonic: wallet.mnemonic.phrase,
+        address: wallet.address,
+        privateKey: wallet.privateKey,
+      },
+    ]);
+  }
 
   function createSolWallet() {
     console.log("Creating Solana wallet");
-    // Implement Solana wallet creation logic here
+    const keypair = Keypair.generate();
+    console.log("Solana Public Key:", keypair.publicKey.toBase58());
+    console.log("Solana Secret Key:", keypair.secretKey.toString());
+
+    const publicKey = keypair.publicKey.toBase58();
+    const secretKey = keypair.secretKey.toString();
+
+    solWallets.push({
+      publicKey: publicKey,
+      secretKey: secretKey,
+    });
+    setSolWallets([
+      ...solWallets,
+      { publicKey: publicKey, secretKey: secretKey },
+    ]);
   }
-
-  const createEthWallet = () => {
-
-    const mnemonic = generateMnemonic();
-    console.log("Generated Mnemonic:", mnemonic);
-    const seed = mnemonicToSeedSync(mnemonic);
-    const path = `m/44'/501'/${account}'/0'`; // This is the derivation path
-    const derivedSeed = derivePath(path, seed.toString("hex")).key;
-    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
-    console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());
-    setAccount(account + 1);
-  };
-
-
 
   return (
     <>
       <h1>Web 3 Wallet</h1>
-      <button onClick={createEthWallet}>Create Ethereum Wallet</button>
-      <button onClick={createSolWallet}>Create Solana Wallet</button>
+      <button onClick={createEthWallet && (<div></div>)}>Create Ethereum Wallet</button>
+
+      <button onClick={createSolWallet && (<div></div>)}>Create Solana Wallet</button>
     </>
   );
 }
